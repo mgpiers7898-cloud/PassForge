96206434d3a1d5ec88e0ee035a82c90aa7798649
Piers DelPiera Amando <mgpiers7898@gmail.com>
2026-06-21 19:22:58 +0330

    Changing the Design

--- src.cpp
+++ src.cpp
@@ -1,42 +1,43 @@
 #include<iostream>
-#include<memory>
 #include<string>
-#include<vector>
 #include<random>
+#include<string_view>
 //one we want the Alphabets and numbers and other Signs also we want operators and strange mix of these things !:
 // so at the first we make the all of these in each var (array) for itration in each one :
 // namespace required for this one is better:
-namespace Characters
+namespace CharactersPool
 {
-    std::vector<char> alphabets{
-        'a', 'b', 'c', 'd', 'e',
-        'f', 'g', 'h', 'i', 'j',
-        'k', 'l', 'm', 'n', 'o',
-        'p', 'q', 'r', 's', 't',
-        'u', 'v', 'w', 'x', 'y',
-        'z'};
-    std::vector<char> capital_alphabets{
-        'A', 'B', 'C', 'D', 'E',
-        'F', 'G', 'H', 'I', 'J',
-        'K', 'L', 'M', 'N', 'O',
-        'P', 'Q', 'R', 'S', 'T',
-        'U', 'V', 'W', 'X', 'Y',
-        'Z'};
-    std::vector<char> signs{
-        '~', '!', '@', '#', '$',
-        '%', '^', '&', '*', '(',
-        ')', '_', '-', '+', '=',
-        '[', ']', '{', '}', ';',
-        ':', '"', ' ', '<', '>',
-        '/', '|', ',', '.'
-    };
-    std::vector<int> numbers
-    {1,2,3,4,5,6,7,8,9,0};
-    
-    // random iteration or memory pool for radomizing through the lower level -> bit by bit?
+    constexpr std::string_view alphabets = "abcdefghijklmnopqrstuvwxyz";
+    constexpr std::string_view capitalAlphabets{"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    constexpr std::string_view numbers{"0123456789"};
+    constexpr std::string_view symbols{"~`!@#$%^&*()_-+={}[];:\"\'<>,?/|\\"};
 };
+// charset will use in switch for the specify the using of Str's
+enum class Charset{
+    Alpha,
+    CapAlpha,
+    Digit,
+    Symbol
+};
+constexpr std::string_view typeSpecify(Charset set){
+    switch(set){
 
-namespace MemoryPool
+        case Charset::Alpha    :    return "Alpha";
+        case Charset::CapAlpha :    return "CapAlpha";
+        case Charset::Digit    :    return "Digit";
+        case Charset::Symbol   :    return "Sign";
+    }
+    return {};
+}
+
+
+
+
+
+
+
+
+namespace GeneratorMethods
 {
     
 };
