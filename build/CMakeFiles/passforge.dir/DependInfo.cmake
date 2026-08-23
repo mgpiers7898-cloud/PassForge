c5642ec996c02293fe92060ed7dc1f9d7cd1e811
Piers DelPiera Amando <mgpiers7898@gmail.com>
2026-08-23 10:08:14 +0330

    Cmake Files config and final Testings Testing

---
+++ build/CMakeFiles/passforge.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Personal Prjects/PassForge/Engines.cpp" "CMakeFiles/passforge.dir/Engines.cpp.obj" "gcc" "CMakeFiles/passforge.dir/Engines.cpp.obj.d"
+  "D:/Personal Prjects/PassForge/Entropy.cpp" "CMakeFiles/passforge.dir/Entropy.cpp.obj" "gcc" "CMakeFiles/passforge.dir/Entropy.cpp.obj.d"
+  "D:/Personal Prjects/PassForge/Facade.cpp" "CMakeFiles/passforge.dir/Facade.cpp.obj" "gcc" "CMakeFiles/passforge.dir/Facade.cpp.obj.d"
+  "D:/Personal Prjects/PassForge/Generator.cpp" "CMakeFiles/passforge.dir/Generator.cpp.obj" "gcc" "CMakeFiles/passforge.dir/Generator.cpp.obj.d"
+  "D:/Personal Prjects/PassForge/Hash.cpp" "CMakeFiles/passforge.dir/Hash.cpp.obj" "gcc" "CMakeFiles/passforge.dir/Hash.cpp.obj.d"
+  "D:/Personal Prjects/PassForge/PasswordPolicy.cpp" "CMakeFiles/passforge.dir/PasswordPolicy.cpp.obj" "gcc" "CMakeFiles/passforge.dir/PasswordPolicy.cpp.obj.d"
+  "D:/Personal Prjects/PassForge/PoolAnalyzer.cpp" "CMakeFiles/passforge.dir/PoolAnalyzer.cpp.obj" "gcc" "CMakeFiles/passforge.dir/PoolAnalyzer.cpp.obj.d"
+  "D:/Personal Prjects/PassForge/PoolBuilder.cpp" "CMakeFiles/passforge.dir/PoolBuilder.cpp.obj" "gcc" "CMakeFiles/passforge.dir/PoolBuilder.cpp.obj.d"
+  "D:/Personal Prjects/PassForge/PoolCompatibility.cpp" "CMakeFiles/passforge.dir/PoolCompatibility.cpp.obj" "gcc" "CMakeFiles/passforge.dir/PoolCompatibility.cpp.obj.d"
+  "D:/Personal Prjects/PassForge/main.cpp" "CMakeFiles/passforge.dir/main.cpp.obj" "gcc" "CMakeFiles/passforge.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/passforge.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/passforge.dir/Engines.cpp.obj"
+  "CMakeFiles/passforge.dir/Engines.cpp.obj.d"
+  "CMakeFiles/passforge.dir/Entropy.cpp.obj"
+  "CMakeFiles/passforge.dir/Entropy.cpp.obj.d"
+  "CMakeFiles/passforge.dir/Facade.cpp.obj"
+  "CMakeFiles/passforge.dir/Facade.cpp.obj.d"
+  "CMakeFiles/passforge.dir/Generator.cpp.obj"
+  "CMakeFiles/passforge.dir/Generator.cpp.obj.d"
+  "CMakeFiles/passforge.dir/Hash.cpp.obj"
+  "CMakeFiles/passforge.dir/Hash.cpp.obj.d"
+  "CMakeFiles/passforge.dir/PasswordPolicy.cpp.obj"
+  "CMakeFiles/passforge.dir/PasswordPolicy.cpp.obj.d"
+  "CMakeFiles/passforge.dir/PoolAnalyzer.cpp.obj"
+  "CMakeFiles/passforge.dir/PoolAnalyzer.cpp.obj.d"
+  "CMakeFiles/passforge.dir/PoolBuilder.cpp.obj"
+  "CMakeFiles/passforge.dir/PoolBuilder.cpp.obj.d"
+  "CMakeFiles/passforge.dir/PoolCompatibility.cpp.obj"
+  "CMakeFiles/passforge.dir/PoolCompatibility.cpp.obj.d"
+  "CMakeFiles/passforge.dir/main.cpp.obj"
+  "CMakeFiles/passforge.dir/main.cpp.obj.d"
+  "libpassforge.dll.a"
+  "passforge.exe"
+  "passforge.exe.manifest"
+  "passforge.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/passforge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
